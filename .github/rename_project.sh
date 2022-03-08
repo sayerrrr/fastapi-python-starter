cabae528f2c3b9e471b5a60146ecadcfcd54cd07
sayerrrr <sayerrrr@users.noreply.github.com>
2022-03-08 20:18:24 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="sayerrrr"
+original_name="fastapi_python_starter"
+original_urlname="fastapi-python-starter"
+original_description="Awesome fastapi_python_starter created by sayerrrr"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv fastapi_python_starter $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- fastapi_python_starter/app.py
+++ fastapi_python_starter/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+fastapi_python_starter API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="fastapi_python_starter",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://fastapi_python_starter.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "sayerrrr",
+        "url": "http://fastapi_python_starter.com/contact/",
+        "email": "sayerrrr@fastapi_python_starter.com",
     },
     license_info={
         "name": "The Unlicense",
